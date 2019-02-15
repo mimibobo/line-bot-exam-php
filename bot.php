f2afc39e5f4714aac347dfb1ebe407dd69be9991
mimibobo <47651062+mimibobo@users.noreply.github.com>
2019-02-15 14:56:49 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -100,11 +100,9 @@
     }
      else {
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
-        $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
-        $arrayPostData['messages'][1]['type'] = "sticker";
-        $arrayPostData['messages'][1]['packageId'] = "1";
-        $arrayPostData['messages'][1]['stickerId'] = "131";
+        $arrayPostData['messages'][0]['type'] = "sticker";
+        $arrayPostData['messages'][0]['packageId'] = "11538";
+        $arrayPostData['messages'][0]['stickerId'] = "51626526";
         replyMsg($arrayHeader,$arrayPostData);
     }
 function replyMsg($arrayHeader,$arrayPostData){
