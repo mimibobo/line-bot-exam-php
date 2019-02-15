0364dafd35f338a8494e9e6ff66988dc7ed4df3e
mimibobo <47651062+mimibobo@users.noreply.github.com>
2019-02-15 11:12:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -17,6 +17,12 @@
         $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
         replyMsg($arrayHeader,$arrayPostData);
     }
+    else if($message == "เวร"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "กรรม";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
     #ตัวอย่าง Message Type "Sticker"
     else if($message == "ฝันดี"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
